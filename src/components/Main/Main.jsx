53125c2ea1352732a84fd4d9c05aefb4f205887e
[ccym90] <[email address]>
2017-04-01 16:51:19 +0800

    graphic

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -33,16 +33,11 @@ class Main extends Component {
         <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
         <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
 
-        <Entity id="box"
+        <Entity id="graphic"
           geometry={{primitive: 'box'}}
           material={{color: this.state.color, opacity: 0.6}}
-          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
           position={{x: 0, y: 1, z: -3}}
           events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
-                  geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
-                  material={{color: '#24CAFF'}}/>
         </Entity>
 
         <Entity primitive="a-camera">
