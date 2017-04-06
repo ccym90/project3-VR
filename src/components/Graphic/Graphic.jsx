9839fb1046216ce8819a0664d0b83cc455574ccc
[ccym90] <[email address]>
2017-04-06 11:11:07 +0800

    entities changing on state

--- src/components/Graphic/Graphic.jsx
+++ src/components/Graphic/Graphic.jsx
@@ -5,21 +5,53 @@ import { showText, showSignPosts } from '../../redux/actions/actions'
 
 class Graphic extends Component {
 
+
   handleClick = (e) => {
-    console.log("huston we have a click");
+    console.log("huston we have a click", this.props.location)
+    console.log("visble???", this.state);
+    this.props.visible === 'false';
     let {dispatch} = this.props;
     dispatch(showText('hk_instruction'));
     console.log("the graphic props", this.props);
     dispatch(showSignPosts());
   }
 
+  renderNewGraphic = () => {
+    console.log("whats my location", this.props.location )
+    if(this.props.location === 'castle') {
+      return(
+        <Entity
+          geometry={{primitive: 'box'}}
+          material={{color: 'red'}}
+          position={{x: 1, y: 1, z: -3}}
+          events={{click: this.handleClick}}/>
+      )
+    } else if (this.props.location === 'darkwoods') {
+      return(
+        <Entity
+        geometry={{primitive: 'box'}}
+        material={{color: 'green'}}
+        position={{x: -1, y:1 , z: -5}}
+        events={{click: this.handleClick}}/>
+      )
+    } else if (this.props.location === 'field') {
+      return(
+        <Entity
+        geometry={{primitive: 'box'}}
+        material={{color: 'yellow'}}
+        position={{x: -3, y: 2, z: -3}}
+        events={{click: this.handleClick}}
+        visible="true"/>
+      )
+    }
+  }
+
+
   render() {
     return (
-      <Entity
-        geometry={{primitive: 'box'}}
-        material={{color: 'blue'}}
-        position={{x: 0, y: 1, z: -3}}
-        events={{click: this.handleClick}}/>
+      <Entity>
+      { this.renderNewGraphic() }
+      </Entity>
     )
   }
 }
