b8b114304933536fba2749798fd56402af9850c0
gwonghk <gwonghk@gmail.com>
2017-04-06 20:11:42 +0800

    minor conf

--- src/components/Graphic/Graphic.jsx
+++ src/components/Graphic/Graphic.jsx
@@ -60,56 +60,6 @@ class Graphic extends Component {
 		}
 	}
 
-<<<<<<< HEAD
-  handleClick3 = (e) => {
-    let {dispatch} = this.props;
-    dispatch(showText('found'));
-    (console.log("CHANGE", this.props.showText))
-  }
-
-  renderNewGraphic = () => {
-    let {dispatch} = this.props;
-    if(this.props.location === 'castle') {
-      return(
-        <Entity
-          geometry={{primitive: 'box', width: 1, height: 1.5, depth: 1}}
-          material={{color: 'red'}}
-          position={{x: -4.14, y: 1, z: -2.81}}
-          rotation={{x:1.72, y:-36.1, z:4.01}}
-          material={{src: '#tomb'}}
-          events={{click: this.handleClick}}
-          </Entity>
-      )
-    } else if (this.props.location === 'darkwoods') {
-      return(
-        <Entity
-        geometry={{primitive: 'box'}}
-        material={{color: 'green'}}
-        position={{x: -1, y:1 , z: -5}}
-        events={{click: this.handleClick2}}/>
-      )
-    } else if (this.props.location === 'field') {
-      // dispatch(showText('field'));
-      return(
-        <Entity
-        geometry={{primitive: 'box'}}
-        material={{color: 'yellow'}}
-        position={{x: -3, y: 2, z: -3}}
-        events={{click: this.handleClick3}}
-        visible="true"/>
-      )
-    }
-  }
-
-
-  render() {
-    return (
-      <Entity>
-      { this.renderNewGraphic() }
-      </Entity>
-    )
-  }
-=======
 	render() {
 		return (
 			<Entity>
@@ -117,7 +67,6 @@ class Graphic extends Component {
 			</Entity>
 		)
 	}
->>>>>>> 4ea3a9b8ed4ff205f1f6dd10e4653b1da051ce85
 }
 
 export default connect(
